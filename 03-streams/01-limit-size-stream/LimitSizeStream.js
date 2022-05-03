1160eb05f37eb8a12a00785b19b0b4f6b11b8691
Maxim Lykov <frise.on@gmail.com>
2022-05-03 17:29:48 +0300

    task-3

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,22 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+
+    this.fullBytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    const chunkBytes = Buffer.byteLength(chunk);
 
+    if (typeof this.limit === 'number' && this.fullBytes + chunkBytes > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      this.fullBytes += chunkBytes;
+
+      callback(null, chunk);
+    }
   }
 }
 
