5c80c9a455f0e5b1a7ae7f01018beaa8a0834941
Maxim Lykov <frise.on@gmail.com>
2022-05-05 23:00:55 +0300

    file-server

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,44 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+
+const isPathnameCorrect = (pathname) => {
+  return pathname && typeof pathname === 'string' && !pathname.includes('/');
+};
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (!isPathnameCorrect(pathname)) {
+    res.statusCode = 400;
+    res.end('Error: pathname is not correct');
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      const fsStream = fs.createReadStream(filepath);
+
+      fsStream.pipe(res);
+
+      fsStream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('Error: file not found');
+        } else {
+          res.statusCode = 500;
+          res.end('Error: something goes wrong');
+        }
+      });
+
+      req.on('aborted', () => {
+        fsStream.destroy();
+      });
 
       break;
 
