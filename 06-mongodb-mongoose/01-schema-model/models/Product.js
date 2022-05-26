f7c0efb0fc73ab323dea7ad68117bf42c5cc69c0
Maxim Lykov <frise.on@gmail.com>
2022-05-26 23:51:18 +0300

    models

--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -2,6 +2,34 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    validate: [{
+      validator: (price) => price > 0,
+      message: 'price should be more than 0',
+    }],
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  subcategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  images: [
+    String,
+  ],
 });
 
 module.exports = connection.model('Product', productSchema);
