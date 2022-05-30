d1253c2337bf9523d8c31128f2411027d6c71bec
Maxim Lykov <frise.on@gmail.com>
2022-05-31 02:14:17 +0300

    authentication-02

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,22 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  let user = await User.findOne({email});
+
+  if (!user) {
+    try {
+      user = await User.create({
+        email,
+        displayName,
+      });
+    } catch (error) {
+      return done(error);
+    }
+  }
+
+  done(null, user);
 };
