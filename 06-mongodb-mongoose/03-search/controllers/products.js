33df8f8dba2012ec003a81dc93d793f4b986bdd7
Maxim Lykov <frise.on@gmail.com>
2022-05-31 00:45:05 +0300

    mongo-search

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,18 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.query;
+
+  if (typeof query !== 'string') {
+    ctx.throw(404, 'query is invalid');
+  }
+
+  const productsData = await Product.find({
+    $text: {
+      $search: query,
+    },
+  });
+
+  ctx.body = {products: productsData.map(mapProduct)};
 };
