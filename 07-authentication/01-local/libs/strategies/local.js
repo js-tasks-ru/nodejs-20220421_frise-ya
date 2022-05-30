fcc50b917444bd085aa33e45bcca55a5618f005f
Maxim Lykov <frise.on@gmail.com>
2022-05-31 01:57:08 +0300

    authentication-01

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,25 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    (email, password, done) => {
+      User.findOne({email}, async (err, user) => {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false, 'Нет такого пользователя');
+        }
+
+        const isCorrectPassword = await user.checkPassword(password);
+
+        if (!isCorrectPassword) {
+          return done(null, false, 'Неверный пароль');
+        }
+
+        return done(null, user);
+      });
     },
 );
